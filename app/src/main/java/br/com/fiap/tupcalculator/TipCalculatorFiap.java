839b22e8f38aa5d04debeae29c35fd131c9258cd
HickAndrade <henrique_andrade2013@hotmail.com>
2019-03-07 11:58:14 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/tupcalculator/TipCalculatorFiap.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tupcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalculatorFiap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator_fiap);
+    }
+}
